d77228eb7f4b206b7d15147acdc18c75b089f481
Ahmed Khan <mahmedkhan.university@gmail.com>
2026-06-15 01:03:27 +0500

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/AKE-Ahmed-Khan-Enterpirses/',
   plugins: [react(), tailwindcss()],
   server: {
     proxy: {
